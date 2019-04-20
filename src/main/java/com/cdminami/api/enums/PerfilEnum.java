4b11a59d3a6be5f0f08cda344e65d042a451ad82
unknown <minamicr@yahoo.com.br>
2019-04-19 23:00:36 -0300

    curso spring udemy

---
+++ src/main/java/com/cdminami/api/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package com.cdminami.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+
+}
---
+++ src/main/java/com/cdminami/api/enums/TipoLancamentoEnum.java
@@ -0,0 +1,11 @@
+package com.cdminami.api.enums;
+
+public enum TipoLancamentoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+
+}
